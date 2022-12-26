d63ef35c5e766b7ba0998d0d3c039267a031ba01
Gayu <54449168+gayuvick@users.noreply.github.com>
2022-12-26 16:12:20 +0530

    Add files via upload

---
+++ scripts/scriptSheet.js
@@ -0,0 +1,37 @@
+
+function knowMe(){
+  var x = document.getElementById("content");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function knowProject(){
+  var x = document.getElementById("project");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function knowRoles(){
+  var x = document.getElementById("roles");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function knowHobby(){
+  var x = document.getElementById("hobby");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
